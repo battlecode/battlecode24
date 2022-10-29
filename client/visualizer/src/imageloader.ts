ab7f7e7630c237ace85727e832d36a47a779d310
Aidan Blum Levine <aidanblumlevine@gmail.com>
2022-10-29 14:55:16 -0400

    adjusting images

--- client/visualizer/src/imageloader.ts
+++ client/visualizer/src/imageloader.ts
@@ -19,14 +19,9 @@ export type AllImages = {
 export function loadAll(config: Config, callback: (arg0: AllImages) => void) {
   const dirname = "./static/img/"
 
-  const NEUTRAL: number = 0;
-  const RED: number = 1;
-  const BLU: number = 2;
-  
-  //To index additional states for buildings
-  const DEFAULT: number = 0;
-  const PORTABLE: number = 1;
-  const PROTOTYPE: number = 2;
+  const NEUTRAL: number = 0
+  const RED: number = 1
+  const BLU: number = 2
 
   function loadImage(
     image: Image,
@@ -35,7 +30,7 @@ export function loadAll(config: Config, callback: (arg0: AllImages) => void) {
   ): void {
     const f = loadImage
     f.expected++
-    
+
     function onFinish() {
       if (f.requestedAll && f.expected == f.success + f.failure) {
         console.log(`Total ${f.expected} images loaded: ${f.success} successful, ${f.failure} failed.`)
@@ -71,12 +66,12 @@ export function loadAll(config: Config, callback: (arg0: AllImages) => void) {
     src?: string
   ): void {
     const image = new Image()
-    loadImage(image, path, src);
-    
+    loadImage(image, path, src)
+
     while (array.length <= index)
-      array.push(new Image());
-    
-    array[index] = image;
+      array.push(new Image())
+
+    array[index] = image
   }
 
   function loadImageInMap(
@@ -85,13 +80,13 @@ export function loadAll(config: Config, callback: (arg0: AllImages) => void) {
     path: string,
     src?: string
   ): void {
-    const image = new Image();
+    const image = new Image()
 
     // Ensure record entry exists
     if (!(key in Map))
-      Map[key] = image;
+      Map[key] = image
 
-    loadImage(image, path, src);
+    loadImage(image, path, src)
   }
 
   function loadImageInArrayMap(
@@ -103,9 +98,9 @@ export function loadAll(config: Config, callback: (arg0: AllImages) => void) {
   ): void {
     // Ensure record entry exists
     if (!(key in Map))
-      Map[key] = [];
+      Map[key] = []
 
-    loadImageInArray(Map[key], arrayIndex, path, src);
+    loadImageInArray(Map[key], arrayIndex, path, src)
   }
 
   const result = {
@@ -115,7 +110,7 @@ export function loadAll(config: Config, callback: (arg0: AllImages) => void) {
     effects: {},
     controls: {}
   }
-  
+
   // helper function to manipulate images
   const htmlToData = (ele: HTMLImageElement): ImageData => {
     const canvas = document.createElement('canvas')
@@ -166,7 +161,7 @@ export function loadAll(config: Config, callback: (arg0: AllImages) => void) {
       const arr = new Uint8ClampedArray(dim ** 2 * 4)
       for (let i = 0; i < arr.length; i += 4) {
         var scale = 8 * (2 + level) / 255
-        var shade = srand() * scale + 1 - scale;
+        var shade = srand() * scale + 1 - scale
         arr[i + 0] = colors[level][0] * shade
         arr[i + 1] = colors[level][1] * shade
         arr[i + 2] = colors[level][2] * shade
@@ -199,115 +194,96 @@ export function loadAll(config: Config, callback: (arg0: AllImages) => void) {
 
   // robot sprites
   for (let team of [RED, BLU]) {
-    let team_str = team == RED ? 'red' : 'blue';
-    for (let level = 1; level <= 3; level++) { 
-      loadImageInArrayMap(result.robots, cst.HEADQUARTERS, level * 6 + DEFAULT * 2 + team, `robots/${team_str}_archon_level${level}`);
-      loadImageInArrayMap(result.robots, cst.CARRIER, level * 6 + DEFAULT * 2 + team, `robots/${team_str}_watchtower_level${level}`);
-      loadImageInArrayMap(result.robots, cst.LAUNCHER, level * 6 + DEFAULT * 2 + team, `robots/${team_str}_lab_level${level}`);
-      loadImageInArrayMap(result.robots, cst.AMPLIFIER, level * 6 + PORTABLE * 2 + team, `robots/${team_str}_archon_portable_level${level}`);
-      loadImageInArrayMap(result.robots, cst.DESTABILIZER, level * 6 + PORTABLE * 2 + team, `robots/${team_str}_watchtower_portable_level${level}`);
-      loadImageInArrayMap(result.robots, cst.BOOSTER, level * 6 + PORTABLE * 2 + team, `robots/${team_str}_lab_portable_level${level}`);
-    }
-    
-    /*
-    loadImage(result.robots.soldier, DEFAULT * 2 + team, `robots/${team_str}_soldier`);
-    loadImage(result.robots.sage, DEFAULT * 2 + team, `robots/${team_str}_sage`);
-    loadImage(result.robots.miner, DEFAULT * 2 + team, `robots/${team_str}_miner`);
-    loadImage(result.robots.builder, DEFAULT * 2 + team, `robots/${team_str}_builder`);
-    loadImage(result.robots.archon, DEFAULT * 2 + team, `robots/${team_str}_archon`);
-    loadImage(result.robots.watchtower, DEFAULT * 2 + team, `robots/${team_str}_watchtower`);
-    loadImage(result.robots.laboratory, DEFAULT * 2 + team, `robots/${team_str}_lab`);
-    loadImage(result.robots.archon, PROTOTYPE * 2 + team, `robots/${team_str}_archon_prototype`);
-    loadImage(result.robots.watchtower, PROTOTYPE * 2 + team, `robots/${team_str}_watchtower_prototype`);
-    loadImage(result.robots.laboratory, PROTOTYPE * 2 + team, `robots/${team_str}_lab_prototype`);
-    */
-  }
-
-  // resources
-  /*
-  loadImage(result.resources, 'lead', 'resources/lead');
-  loadImage(result.resources, 'gold', 'resources/gold');
-  */
-
-  // effects
-  // loadImage(result.effects, 'death', 'effects/death/death_empty');
-  // loadImage(result.effects.embezzle, 0, 'effects/embezzle/slanderer_embezzle_empty_1');
-  // loadImage(result.effects.embezzle, 1, 'effects/embezzle/slanderer_embezzle_empty_2');
-
-  {
-    const makeTransparent = (data: ImageData): ImageData => {
-      const arr = new Uint8ClampedArray(data.data.length)
-      for (let i = 0; i < arr.length; i += 4) {
-        arr[i + 0] = data.data[i + 0]
-        arr[i + 1] = data.data[i + 1]
-        arr[i + 2] = data.data[i + 2]
-        arr[i + 3] = data.data[i + 3] / 1.4
-      }
-      const result = new ImageData(arr, data.width)
-      return result
-    }
-
-    const makeRed = (data: ImageData): ImageData => {
-      const arr = new Uint8ClampedArray(data.data.length)
-      for (let i = 0; i < arr.length; i += 4) {
-        arr[i + 0] = 255
-        arr[i + 1] = 0
-        arr[i + 2] = 0
-        arr[i + 3] = data.data[i + 3]
-      }
-      const result = new ImageData(arr, data.width)
-      return result
-    }
-
-    const makeBlue = (data: ImageData): ImageData => {
-      const arr = new Uint8ClampedArray(data.data.length)
-      for (let i = 0; i < arr.length; i += 4) {
-        arr[i + 0] = 0
-        arr[i + 1] = 0
-        arr[i + 2] = 255
-        arr[i + 3] = data.data[i + 3]
-      }
-      const result = new ImageData(arr, data.width)
-      return result
-    }
-
-    for (let i = 0; i < 2; i++) {
-      const base: Image = new Image()
-      base.src = require(dirname + `effects/empower/polit_empower_empty_${i + 1}.png`).default
-
-      base.onload = () => {
-        const data: ImageData = htmlToData(base)
-        const trans: ImageData = makeTransparent(data)
-        const red: ImageData = makeRed(trans)
-        const blue: ImageData = makeBlue(trans)
-        const path_red: String = dataToSrc(red)
-        const path_blue: String = dataToSrc(blue)
-        // loadImage(result.effects.empower_red, i, "", path_red.slice(0, path_red.length - 4))
-        // loadImage(result.effects.empower_blue, i, "", path_blue.slice(0, path_blue.length - 4))
-      }
+    let team_str = team == RED ? 'red' : 'blue'
+    for (let bodytype of cst.bodyTypeList) {
+      loadImageInArrayMap(result.robots, bodytype, team, `robots/${team_str}_${cst.bodyTypeToString(bodytype)}`)
     }
   }
+// resources
+/*
+loadImage(result.resources, 'lead', 'resources/lead');
+loadImage(result.resources, 'gold', 'resources/gold');
+*/
 
-  /*
-  loadImage(result.effects.expose, 0, 'effects/expose/expose_empty')
-  loadImage(result.effects.camouflage_red, 0, 'effects/camouflage/camo_red')
-  loadImage(result.effects.camouflage_blue, 0, 'effects/camouflage/camo_blue')
-  */
+// effects
+// loadImage(result.effects, 'death', 'effects/death/death_empty');
+// loadImage(result.effects.embezzle, 0, 'effects/embezzle/slanderer_embezzle_empty_1');
+// loadImage(result.effects.embezzle, 1, 'effects/embezzle/slanderer_embezzle_empty_2');
 
-  // load controls
-  // buttons are from https://material.io/resources/icons
-  loadImageInMap(result.controls, ControlType.GO_NEXT, 'controls/go-next')
-  loadImageInMap(result.controls, ControlType.GO_PREVIOUS, 'controls/go-previous')
-  loadImageInMap(result.controls, ControlType.PLAYBACK_PAUSE, 'controls/playback-pause')
-  loadImageInMap(result.controls, ControlType.PLAYBACK_START, 'controls/playback-start')
-  loadImageInMap(result.controls, ControlType.PLAYBACK_STOP, 'controls/playback-stop')
-  loadImageInMap(result.controls, ControlType.REVERSE_UPS, 'controls/reverse')
-  loadImageInMap(result.controls, ControlType.DOUBLE_UPS, 'controls/skip-forward')
-  loadImageInMap(result.controls, ControlType.HALVE_UPS, 'controls/skip-backward')
-  loadImageInMap(result.controls, ControlType.GO_END, 'controls/go-end')
-  loadImageInMap(result.controls, ControlType.MATCH_BACKWARD, 'controls/green-previous')
-  loadImageInMap(result.controls, ControlType.MATCH_FORWARD, 'controls/green-next')
+{
+  const makeTransparent = (data: ImageData): ImageData => {
+    const arr = new Uint8ClampedArray(data.data.length)
+    for (let i = 0; i < arr.length; i += 4) {
+      arr[i + 0] = data.data[i + 0]
+      arr[i + 1] = data.data[i + 1]
+      arr[i + 2] = data.data[i + 2]
+      arr[i + 3] = data.data[i + 3] / 1.4
+    }
+    const result = new ImageData(arr, data.width)
+    return result
+  }
 
-  // mark as finished
-  loadImage.requestedAll = true
+  const makeRed = (data: ImageData): ImageData => {
+    const arr = new Uint8ClampedArray(data.data.length)
+    for (let i = 0; i < arr.length; i += 4) {
+      arr[i + 0] = 255
+      arr[i + 1] = 0
+      arr[i + 2] = 0
+      arr[i + 3] = data.data[i + 3]
+    }
+    const result = new ImageData(arr, data.width)
+    return result
+  }
+
+  const makeBlue = (data: ImageData): ImageData => {
+    const arr = new Uint8ClampedArray(data.data.length)
+    for (let i = 0; i < arr.length; i += 4) {
+      arr[i + 0] = 0
+      arr[i + 1] = 0
+      arr[i + 2] = 255
+      arr[i + 3] = data.data[i + 3]
+    }
+    const result = new ImageData(arr, data.width)
+    return result
+  }
+
+  for (let i = 0; i < 2; i++) {
+    const base: Image = new Image()
+    base.src = require(dirname + `effects/empower/polit_empower_empty_${i + 1}.png`).default
+
+    base.onload = () => {
+      const data: ImageData = htmlToData(base)
+      const trans: ImageData = makeTransparent(data)
+      const red: ImageData = makeRed(trans)
+      const blue: ImageData = makeBlue(trans)
+      const path_red: String = dataToSrc(red)
+      const path_blue: String = dataToSrc(blue)
+      // loadImage(result.effects.empower_red, i, "", path_red.slice(0, path_red.length - 4))
+      // loadImage(result.effects.empower_blue, i, "", path_blue.slice(0, path_blue.length - 4))
+    }
+  }
+}
+
+/*
+loadImage(result.effects.expose, 0, 'effects/expose/expose_empty')
+loadImage(result.effects.camouflage_red, 0, 'effects/camouflage/camo_red')
+loadImage(result.effects.camouflage_blue, 0, 'effects/camouflage/camo_blue')
+*/
+
+// load controls
+// buttons are from https://material.io/resources/icons
+loadImageInMap(result.controls, ControlType.GO_NEXT, 'controls/go-next')
+loadImageInMap(result.controls, ControlType.GO_PREVIOUS, 'controls/go-previous')
+loadImageInMap(result.controls, ControlType.PLAYBACK_PAUSE, 'controls/playback-pause')
+loadImageInMap(result.controls, ControlType.PLAYBACK_START, 'controls/playback-start')
+loadImageInMap(result.controls, ControlType.PLAYBACK_STOP, 'controls/playback-stop')
+loadImageInMap(result.controls, ControlType.REVERSE_UPS, 'controls/reverse')
+loadImageInMap(result.controls, ControlType.DOUBLE_UPS, 'controls/skip-forward')
+loadImageInMap(result.controls, ControlType.HALVE_UPS, 'controls/skip-backward')
+loadImageInMap(result.controls, ControlType.GO_END, 'controls/go-end')
+loadImageInMap(result.controls, ControlType.MATCH_BACKWARD, 'controls/green-previous')
+loadImageInMap(result.controls, ControlType.MATCH_FORWARD, 'controls/green-next')
+
+// mark as finished
+loadImage.requestedAll = true
 }
