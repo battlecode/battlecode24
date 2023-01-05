5ef8bdf48c63b46fe5cc965e3ddde8f412d80c68
Aidan Blum Levine <aidanblumlevine@gmail.com>
2023-01-05 15:43:40 -0500

    no path for headquarters

--- client/visualizer/src/gamearea/renderer.ts
+++ client/visualizer/src/gamearea/renderer.ts
@@ -390,6 +390,7 @@ export default class Renderer {
   private renderPath(world: GameWorld) {
     const path = world.pathHistory.get(this.lastSelectedID);
     if (!path || path.length < 2) return
+    if(world.bodies.lookup(this.lastSelectedID).type == cst.HEADQUARTERS) return
     const ctx = this.ctx[CanvasType.DYNAMIC]
     const height = world.maxCorner.y - world.minCorner.y
     
