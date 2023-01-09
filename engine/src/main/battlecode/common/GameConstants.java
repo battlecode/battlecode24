1dd7453426093e7eb16849582e49e10b4a2c4972
Andy Wang <andywang02@gmail.com>
2023-01-09 18:10:02 -0500

    Updating version release to 0.1.7

--- engine/src/main/battlecode/common/GameConstants.java
+++ engine/src/main/battlecode/common/GameConstants.java
@@ -10,7 +10,7 @@ public class GameConstants {
     /**
      * The current spec version the server compiles with.
      */
-    public static final String SPEC_VERSION = "0.1.1";
+    public static final String SPEC_VERSION = "0.1.7";
 
     // *********************************
     // ****** MAP CONSTANTS ************
