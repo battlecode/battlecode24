bac9d80df4d9bb06843a4a128f890430f6db61af
Pranali <pvani@mit.edu>
2023-01-09 19:06:40 -0500

    Updated version numbers

--- engine/src/main/battlecode/common/GameConstants.java
+++ engine/src/main/battlecode/common/GameConstants.java
@@ -10,7 +10,7 @@ public class GameConstants {
     /**
      * The current spec version the server compiles with.
      */
-    public static final String SPEC_VERSION = "0.1.7";
+    public static final String SPEC_VERSION = "1.0.0";
 
     // *********************************
     // ****** MAP CONSTANTS ************
