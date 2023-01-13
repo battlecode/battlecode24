6022f7b3ae6c6e5f9034956d05c94fd80b0446cb
Jerry Mao <j-mao@users.noreply.github.com>
2023-01-13 02:25:22 -0500

    Release v1.2.3

--- engine/src/main/battlecode/common/GameConstants.java
+++ engine/src/main/battlecode/common/GameConstants.java
@@ -10,7 +10,7 @@ public class GameConstants {
     /**
      * The current spec version the server compiles with.
      */
-    public static final String SPEC_VERSION = "1.2.2";
+    public static final String SPEC_VERSION = "1.2.3";
 
     // *********************************
     // ****** MAP CONSTANTS ************
