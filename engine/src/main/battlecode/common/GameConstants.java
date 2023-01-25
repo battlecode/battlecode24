2ec5925ab7ce112d38dc86c09a47e629982f9ed2
Andy Wang <wangandy@mit.edu>
2023-01-25 05:11:36 -0500

    Version 3.0.1 spec fixes

--- engine/src/main/battlecode/common/GameConstants.java
+++ engine/src/main/battlecode/common/GameConstants.java
@@ -10,7 +10,7 @@ public class GameConstants {
     /**
      * The current spec version the server compiles with.
      */
-    public static final String SPEC_VERSION = "3.0.0";
+    public static final String SPEC_VERSION = "3.0.1";
 
     // *********************************
     // ****** MAP CONSTANTS ************
