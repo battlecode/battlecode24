fc43fa01295ffb449e57c3abaceafb2ed0e62892
eraserbird <ruthluvu@mit.edu>
2023-10-15 16:46:14 -0400

    added global upgrades and trap enums

---
+++ engine/src/main/battlecode/common/SkillType.java
@@ -0,0 +1,15 @@
+package battlecode.common;
+
+/**
+ * Enumerates the different skill types a unit can specialize in.
+ */
+
+public enum SkillType{
+
+    // Exp req levels 1-6, specialization effects levels 1-6, jailed penalty level 1-6
+    ATTACK(),
+    BUILD(),
+    HEAL();
+
+
+}
