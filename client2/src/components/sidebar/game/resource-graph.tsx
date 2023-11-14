3764101f326d0eb227c9d55ac090294bb35290b6
Aidan Blum Levine <aidanblumlevine@gmail.com>
2023-11-14 10:46:43 -0500

    Undo caching

--- client2/src/components/sidebar/game/resource-graph.tsx
+++ client2/src/components/sidebar/game/resource-graph.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useMemo, useRef, useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 import { AppContext, useAppContext } from '../../../app-context'
 import { useListenEvent, EventType } from '../../../app-events'
@@ -11,51 +11,52 @@ interface Props {
     property: string
     propertyDisplayName: string
 }
-
 function hasKey<O extends Object>(obj: O, key: PropertyKey): key is keyof O {
     return key in obj
 }
 
-export const ResourceGraph: React.FC<Props> = (props: Props) => {
-    const cachedData = useRef<DataPoint[]>([])
-    const appContext = useAppContext()
+function getChartData(appContext: AppContext, property: string): DataPoint[] {
     const match = appContext.state.activeMatch
-    useEffect(() => {
-        cachedData.current = []
-    }, [match])
-
-    const forceUpdate = useForceUpdate()
-    useListenEvent(EventType.TURN_PROGRESS, forceUpdate)
-
-    if (match && match.currentTurn.turnNumber > cachedData.current.length) {
-        for (let i = cachedData.current.length; i < match.currentTurn.turnNumber; i++) {
-            const redTeamStat = match.stats[i].getTeamStat(match.game.teams[0])
-            const blueTeamStat = match.stats[i].getTeamStat(match.game.teams[1])
-            assert(hasKey(redTeamStat, props.property))
-            assert(hasKey(blueTeamStat, props.property))
-            cachedData.current.push({
-                turn: i + 1,
-                red: redTeamStat[props.property] as number,
-                blue: blueTeamStat[props.property] as number
-            })
-        }
+    if (match === undefined) {
+        return []
     }
 
-    const displayData = cachedData.current.slice(0, match ? match.currentTurn.turnNumber : 0)
+    const values = [0, 1].map((index) =>
+        match.stats.map((turnStat) => {
+            const teamStat = turnStat.getTeamStat(match.game.teams[index])
+            assert(hasKey(teamStat, property))
+            return teamStat[property]
+        })
+    )
+
+    return values[0].slice(0, match.currentTurn.turnNumber).map((value, index) => {
+        return {
+            turn: index + 1,
+            red: value as number,
+            blue: values[1][index] as number
+        }
+    })
+}
+
+export const ResourceGraph: React.FC<Props> = (props: Props) => {
+    const appContext = useAppContext()
+    const forceUpdate = useForceUpdate()
+
+    useListenEvent(EventType.TURN_PROGRESS, forceUpdate)
 
     return (
         <div className="mt-2 px-2 w-full">
             <h2 className="mx-auto text-center">{props.propertyDisplayName}</h2>
-            {/* <ResponsiveContainer aspect={1.5} width="100%" className="text-xs"> */}
-            {/* <div className="App"> */}
-            <D3LineChart
-                data={displayData}
-                width={300 + 40} // Add 40 so that tooltip is visible outside of SVG container
-                height={300}
-                margin={{ top: 20, right: 20 + 20, bottom: 30, left: 40 + 20 }}
-            />
-            {/* </div> */}
-            {/* </ResponsiveContainer> */}
+            <ResponsiveContainer aspect={1.5} width="100%" className="text-xs">
+                <div className="App">
+                    <D3LineChart
+                        data={getChartData(appContext, props.property)}
+                        width={300 + 40} // Add 40 so that tooltip is visible outside of SVG container
+                        height={300}
+                        margin={{ top: 20, right: 20 + 20, bottom: 30, left: 40 + 20 }}
+                    />
+                </div>
+            </ResponsiveContainer>
         </div>
     )
 }
