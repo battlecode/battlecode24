3f5389c385b29246b725b7ff8c4548ac505f2328
hank <hpstennes@gmail.com>
2023-12-02 15:35:45 -0500

    Delete more stuff

--- engine/src/test/battlecode/world/GenerateMaps.java
+++ engine/src/test/battlecode/world/GenerateMaps.java
@@ -21,24 +21,7 @@ import java.io.IOException;
 public class GenerateMaps {
     @Test
     public void makeSimple() throws IOException {
-        LiveMap map = new TestMapBuilder("maptest", 0, 0, 100, 100, 30)
-                .addHeadquarters(
-                        0,
-                        Team.A,
-                        new MapLocation(
-                                1,
-                                1
-                        )
-                )
-                .addHeadquarters(
-                        1,
-                        Team.B,
-                        new MapLocation(
-                                99,
-                                99
-                        )
-                )
-                .build();
+        LiveMap map = new TestMapBuilder("maptest", 0, 0, 100, 100, 30).build();
         boolean teamsReversed = false;
         GameMapIO.writeMap(map, new File("/Users/ezou/dev/battlecode20/engine/src/main/battlecode/world/maptest"));
         LiveMap test = GameMapIO.loadMap("maptest", new File("/Users/ezou/dev/battlecode20/engine/src/main/battlecode/world/resources"), teamsReversed);
