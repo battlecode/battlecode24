c0c244375d27de198a0f3fe6fad106e7cbd7272f
TheApplePieGod <evant.002@gmail.com>
2023-12-15 18:03:36 -0500

    Tauri backend implementation

--- client2/src-electron/electron-bridge.js
+++ client2/src-electron/electron-bridge.js
@@ -1,4 +1,4 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron')
 
 const electronAPI = {
     openScaffoldDirectory: (...args) => ipcRenderer.invoke('electronAPI', 'openScaffoldDirectory', ...args),
@@ -7,20 +7,29 @@ const electronAPI = {
         join: (...args) => ipcRenderer.invoke('electronAPI', 'path.join', ...args),
         relative: (...args) => ipcRenderer.invoke('electronAPI', 'path.relative', ...args),
         dirname: (...args) => ipcRenderer.invoke('electronAPI', 'path.dirname', ...args),
-        getSeperator: (...args) => ipcRenderer.invoke('electronAPI', 'path.sep', ...args),
+        getSeperator: (...args) => ipcRenderer.invoke('electronAPI', 'path.sep', ...args)
     },
     fs: {
         exists: (...args) => ipcRenderer.invoke('electronAPI', 'fs.existsSync', ...args),
         mkdir: (...args) => ipcRenderer.invoke('electronAPI', 'fs.mkdirSync', ...args),
-        getFiles: (...args) => ipcRenderer.invoke('electronAPI', 'fs.getFiles', ...args),
+        getFiles: (...args) => ipcRenderer.invoke('electronAPI', 'fs.getFiles', ...args)
     },
     child_process: {
         spawn: (...args) => ipcRenderer.invoke('electronAPI', 'child_process.spawn', ...args),
         kill: (...args) => ipcRenderer.invoke('electronAPI', 'child_process.kill', ...args),
-        onStdout: (callback) => ipcRenderer.on('child_process.stdout', (event, data) => { callback(data); }),
-        onStderr: (callback) => ipcRenderer.on('child_process.stderr', (event, data) => { callback(data); }),
-        onExit: (callback) => ipcRenderer.on('child_process.exit', (event, data) => { callback(data); }),
-    },
-};
+        onStdout: (callback) =>
+            ipcRenderer.on('child_process.stdout', (event, data) => {
+                callback(data)
+            }),
+        onStderr: (callback) =>
+            ipcRenderer.on('child_process.stderr', (event, data) => {
+                callback(data)
+            }),
+        onExit: (callback) =>
+            ipcRenderer.on('child_process.exit', (event, data) => {
+                callback(data)
+            })
+    }
+}
 
-contextBridge.exposeInMainWorld('electronAPI', electronAPI);
+contextBridge.exposeInMainWorld('electronAPI', electronAPI)
