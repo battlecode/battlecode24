a4f270fc987929dbebcf8485b1d7e6e2945b190c
hank <hpstennes@gmail.com>
2023-12-15 22:22:08 -0500

    Add senseLegalStartingFlagPlacement

--- example-bots/src/main/HankPlayer/RobotPlayer.java
+++ example-bots/src/main/HankPlayer/RobotPlayer.java
@@ -144,7 +144,7 @@ public class RobotPlayer {
         if(rc.getLocation() == null) System.out.println("I am alive but my location is null 2");
         RobotInfo[] enemies = rc.senseNearbyRobots(GameConstants.ACTION_RADIUS_SQUARED, rc.getTeam().opponent());
         if (enemies.length != 0 && rc.canAttack(enemies[0].getLocation())){
-            // rc.attack(enemies[0].getLocation());
+            rc.attack(enemies[0].getLocation());
             //System.out.println("punched someone");
         } 
     }
