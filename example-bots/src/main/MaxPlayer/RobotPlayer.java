a0213244b26e21c7839e0d2ceb0ba8d18cc9a6c3
Max Misterka <maxmisterka64@gmail.com>
2023-12-15 21:01:05 -0500

    Made MaxPlayer actually move

--- example-bots/src/main/MaxPlayer/RobotPlayer.java
+++ example-bots/src/main/MaxPlayer/RobotPlayer.java
@@ -19,20 +19,52 @@ public class RobotPlayer {
     };
 
     public static void run(RobotController rc) throws GameActionException {
-        System.out.println("I'm alive");
-        rc.setIndicatorString("Hello world!");
-
         while (true) {
             try {
                 if (rc.isSpawned()) {
-                    throw new Exception("MaxPlayer TODO");
+                    int numGoodDirs = 0;
+                    Direction[] goodDirs = new Direction[directions.length];
+
+                    for (Direction dir : directions) {
+                        // MapLocation newLoc = rc.adjacentLocation(dir);
+
+                        // if (!rc.onTheMap(newLoc) || rc.isLocationOccupied(newLoc)) {
+                        //     break;
+                        // }
+
+                        if (rc.canMove(dir)) {
+                            goodDirs[numGoodDirs] = dir;
+                            numGoodDirs++;
+                        }
+                    }
+
+                    if (numGoodDirs > 0) {
+                        int rand = rng.nextInt(numGoodDirs);
+                        Direction moveDir = directions[rand];
+                        rc.move(moveDir);
+                    }
                 } else {
-                    throw new Exception("MaxPlayer TODO");
+                    MapLocation[] spawnZoneLocs = rc.getAllySpawnLocations();
+                    MapLocation[] goodLocs = new MapLocation[spawnZoneLocs.length];
+                    int numGoodLocs = 0;
+
+                    for (MapLocation loc : spawnZoneLocs) {
+                        if (rc.canSpawn(loc)) {
+                            goodLocs[numGoodLocs] = loc;
+                            numGoodLocs++;
+                        }
+                    }
+
+                    if (numGoodLocs > 0) {
+                        int rand = rng.nextInt(numGoodLocs);
+                        MapLocation spawnLoc = goodLocs[rand];
+                        rc.spawn(spawnLoc);
+                    }
                 }
             } catch (GameActionException e) {
                 System.out.println("GameActionException! Message: " + e.getMessage());
             } catch (Exception e) {
-                System.out.println("Other Exception! Message: " + e.getMessage());
+                System.out.println("Other Exception! Type: " + e.getClass() + " Message: " + e.getMessage());
             } finally {
                 Clock.yield();
             }
