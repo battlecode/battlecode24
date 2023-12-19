877a70039fd911821dc73c8eba8264729cb3fedc
Aidan Blum Levine <aidanblumlevine@gmail.com>
2023-12-19 17:18:03 -0500

    Fix runner maps

--- client/src/components/forms.tsx
+++ client/src/components/forms.tsx
@@ -8,13 +8,28 @@ interface SelectProps {
 
 export const Select: React.FC<PropsWithChildren<SelectProps>> = (props) => {
     return (
-        <select
-            className={props.className + ' border border-black py-1 px-1 rounded-md'}
-            value={props.value}
-            onChange={(e) => props.onChange(e.target.value)}
-        >
-            {props.children}
-        </select>
+        <div className="relative">
+            <select
+                className={props.className + ' appearance-none border border-black py-1 px-1 rounded-md w-full h-full'}
+                value={props.value}
+                onChange={(e) => props.onChange(e.target.value)}
+            >
+                {props.children}
+            </select>
+            <div className="absolute inset-y-0 right-0 flex items-center mx-1 my-2 bg-white bg-opacity-75 pointer-events-none">
+                <svg
+                    className="w-5 h-5 fill-current text-black"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                >
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M6.293 7.293a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                    />
+                </svg>
+            </div>
+        </div>
     )
 }
 
