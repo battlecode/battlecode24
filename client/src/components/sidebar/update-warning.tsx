603a4cd8ae27970f5b07369eef36aeede535e2f4
TheApplePieGod <evant.002@gmail.com>
2024-01-05 15:21:44 -0600

    Fix update warning error

--- client/src/components/sidebar/update-warning.tsx
+++ client/src/components/sidebar/update-warning.tsx
@@ -1,23 +1,25 @@
 import React, { useEffect } from 'react'
 import { BATTLECODE_YEAR, GAME_VERSION } from '../../constants'
+import { nativeAPI } from './runner/native-api-wrapper'
 
 export const UpdateWarning = () => {
     const [update, setUpdate] = React.useState<undefined | { latest: string }>(undefined)
 
     useEffect(() => {
-        try {
-            fetch(`https://api.battlecode.org/api/episode/e/bc${BATTLECODE_YEAR % 100}/?format=json`)
-                .then((response) => response.json())
-                .then((json) => {
-                    const latest = json.release_version_public
-                    if (latest.trim() != GAME_VERSION) {
-                        setUpdate({ latest })
-                    }
-                })
-        } catch (e) {
-            console.log(e)
-        }
-    })
+        // Don't need to check for version updates in the web client
+        if (!nativeAPI) return
+
+        const url = `https://api.battlecode.org/api/episode/e/bc${BATTLECODE_YEAR % 100}/?format=json`
+        fetch(url, { mode: 'no-cors' })
+            .then((response) => response.json())
+            .then((json) => {
+                const latest = json.release_version_public
+                if (latest.trim() != GAME_VERSION) {
+                    setUpdate({ latest })
+                }
+            })
+            .catch((e) => console.log(e))
+    }, [])
 
     if (!update) return null
 
