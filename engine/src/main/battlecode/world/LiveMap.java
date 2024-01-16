207ba59fb4c604ca9ca14ffc4849e0d8e7f4a5d8
hank <hpstennes@gmail.com>
2024-01-15 23:52:15 -0500

    Remove dams and crumbs map guarantee

--- engine/src/main/battlecode/world/LiveMap.java
+++ engine/src/main/battlecode/world/LiveMap.java
@@ -407,20 +407,15 @@ public strictfp class LiveMap {
                 } 
             }
             if(this.damArray[i]) {
-                if(this.breadArray[i] != 0) {
-                    throw new RuntimeException("Dams can't be on the same square as bread.");
-                }
                 if(this.spawnZoneArray[i] != 0) {
                     throw new RuntimeException("Dams can't be on the same square as spawn zones.");
                 }
             }
-
             if(this.waterArray[i]) {
                 if(this.spawnZoneArray[i] != 0) {
                     throw new RuntimeException("Water can't be on the same square as spawn zones.");
                 }
             }
-
         }
         assertSpawnZoneDistances();
         assertSpawnZonesAreValid();
